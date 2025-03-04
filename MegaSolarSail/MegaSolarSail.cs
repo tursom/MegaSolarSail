4dbb539c7674acffd524e7e2b019cb2fa4e90ce4
tursom <tursom@foxmail.com>
2025-03-04 22:41:23 +0800

    创建项目

---
+++ MegaSolarSail/MegaSolarSail.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BepInEx;
+using BepInEx.Configuration;
+
+namespace MegaSolarSail
+{
+
+    [BepInPlugin("cn.tursom.MegaSolarSail", "MegaSolarSail", "0.0.1")]
+    public class MegaSolarSail : BaseUnityPlugin
+    {
+        private static ConfigEntry<double> EnergyMulCfg;
+        private static double EnergyMul => EnergyMulCfg.Value;
+
+        void Start()
+        {
+            Logger.LogInfo("MegaSolarSail on load");
+
+            LoadConfig();
+
+            Configs.freeMode.solarSailEnergyPerTick = (long)(Configs.freeMode.solarSailEnergyPerTick * EnergyMul);
+            Configs.freeMode.dysonNodeEnergyPerTick = (long)(Configs.freeMode.dysonNodeEnergyPerTick * EnergyMul);
+            Configs.freeMode.dysonFrameEnergyPerTick = (long)(Configs.freeMode.dysonFrameEnergyPerTick * EnergyMul);
+            Configs.freeMode.dysonShellEnergyPerTick = (long)(Configs.freeMode.dysonShellEnergyPerTick * EnergyMul);
+        }
+
+        private void LoadConfig()
+        {
+            EnergyMulCfg = Config.Bind("config", "EnergyMul", 10.0, "solar sail energy increase multiplier\n"
+                                                                    + "太阳帆能量增强倍率");
+            Config.Save();
+        }
+    }
+}

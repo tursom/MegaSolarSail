16c539237cd14c8000b9ccfb20055f5a56cae762
tursom <tursom@foxmail.com>
2025-03-04 23:06:58 +0800

    update mod version to 1.0.0

--- MegaSolarSail/MegaSolarSail.cs
+++ MegaSolarSail/MegaSolarSail.cs
@@ -5,7 +5,7 @@ using BepInEx.Configuration;
 namespace MegaSolarSail
 {
 
-    [BepInPlugin("cn.tursom.MegaSolarSail", "MegaSolarSail", "0.0.1")]
+    [BepInPlugin("cn.tursom.MegaSolarSail", "MegaSolarSail", "1.0.0")]
     public class MegaSolarSail : BaseUnityPlugin
     {
 #pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑添加 "required" 修饰符或声明为可为 null。
